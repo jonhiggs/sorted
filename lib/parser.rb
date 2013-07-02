68bf096f6540f0acf41d674fdcbd71b5bbc28709
Jon Higgs <jhiggs0@gmail.com>
2013-07-02 23:02:52 +0200

    determine the depth of each line.

--- lib/parser.rb
+++ lib/parser.rb
@@ -25,5 +25,30 @@ module Sorted
       find_indentations.last.empty? ? "" : find_indentations.last.match(/^./).to_s
     end
 
+    def output
+      out = []
+      input.each do |line|
+        out.push({ :depth => depth(line), :data => clean(line) })
+      end
+      out
+    end
+
+    private
+    def depth line
+      return 0 if indentation_value.empty?
+      line.scan(/^#{indentation_value}{#{indentation_count}}/).size
+    end
+
+    private
+    def clean line
+      return line if indentation_value.empty?
+      while line.scan(/^#{indentation_value}{#{indentation_count}}/).size > 0
+        line.gsub!(/^#{indentation_value}{#{indentation_count}}/, "")
+        puts line
+        break if indentation_value.empty?
+      end
+      line
+    end
+
   end
 end
--- test/test_parser.rb
+++ test/test_parser.rb
@@ -17,6 +17,7 @@ flat file
   asserts(:find_indentations).equals [""]
   asserts(:indentation_count).equals 0
   asserts(:indentation_value).equals ""
+  asserts(:output).equals [ { :depth => 0, :data => "this is a"}, {:depth => 0, :data => "very"}, { :depth => 0, :data => "flat file"}]
 end
 
 context "#Parser::Indentation - Complete File" do
@@ -42,6 +43,17 @@ end
 context "#Parser::Indentation - Tab Indented File" do
   setup do
     @stdin = "first level\n\tinside first level\n"
+    obj = Sorted::Parser.new(@stdin)
+  end
+
+  asserts(:indentation_count).equals 1
+  asserts(:indentation_value).equals "\t"
+  asserts(:output).equals [ { :depth => 0, :data => "first level" }, { :depth => 1, :data => "inside first level"} ]
+end
+
+context "#Parser::Indentation - File gets shallower" do
+  setup do
+    @stdin = "\t\treally deep\n\tdeep\ntop\n"
     obj = Sorted::Parser.new(@stdin)
   end
 
