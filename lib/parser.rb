0a9fc5182e377d8d47414dc4461ce10593292d06
Jon Higgs <jhiggs0@gmail.com>
2013-07-02 21:53:00 +0200

    determine the indentation char and count.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,10 +1,29 @@
 module Sorted
   class Parser
     def initialize input
-      @input = input
+      @input = input.split("\n")
     end
 
     def input() @input end
+
+    def find_indentations
+      whitespaces = []
+      input.each do |line|
+        v = line.match(/^\s*/).to_s
+        whitespaces.push(v)
+      end
+      whitespaces.sort.uniq
+    end
+
+    def indentation_count
+      indents = find_indentations
+      return indents.first.size if indents.first.size == indents.last.size
+      indents[-1].size - indents[-2].size
+    end
+
+    def indentation_value
+      find_indentations.last.empty? ? "" : find_indentations.last.match(/^./).to_s
+    end
 
   end
 end
--- test/test_parser.rb
+++ test/test_parser.rb
@@ -1,6 +1,7 @@
 require 'parser'
 require 'test_helper'
 
+### COMPLETE FILES ############################################################
 context "#Parser::Indentation - Flat File" do
   setup do
     @stdin = <<-EOF
@@ -12,24 +13,38 @@ flat file
     obj = Sorted::Parser.new(@stdin)
   end
 
-  asserts(:input).equals "this is a\nvery\nflat file\n"
+  asserts(:input).equals ["this is a", "very", "flat file"]
+  asserts(:find_indentations).equals [""]
+  asserts(:indentation_count).equals 0
+  asserts(:indentation_value).equals ""
 end
 
-#  asserts(:slug).equals "the-wizard-of-oz"
-#  asserts(:summary).equals "Once upon a time&hellip;"
-#  asserts(:url).equals "http://127.0.0.1/article_directory/the-wizard-of-oz/"
-#  asserts(:path).equals "/article_directory/the-wizard-of-oz/"
-#  asserts(:git_history).equals "https://github.com/gituser/gitrepo/commits/master/markdown/the-dichotomy-of-design.txt"
-#  asserts(:git_source).equals "https://raw.github.com/gituser/gitrepo/master/markdown/the-dichotomy-of-design.txt"
-#  asserts(:original).equals true
-#  asserts(:tags).equals []
-#  asserts(:categories).equals []
-#  asserts(:comments).nil
-#  asserts(:modified).equals "12/10/1932"
-#  asserts(:source_name).nil
-#  #asserts(:source_url).nil
-#  asserts(:title).equals "the wizard of oz"
-#  asserts(:date).equals "12/10/1932"
-#  asserts(:author).equals ENV["USER"]
-#  asserts(:summary).equals "Once upon a time&hellip;"
-#
+context "#Parser::Indentation - Complete File" do
+  setup do
+    @stdin = <<-EOF
+first level
+  inside first level
+    a second nested value
+      a third nested value
+another at top
+  inside another
+    EOF
+
+    obj = Sorted::Parser.new(@stdin)
+  end
+
+  asserts(:find_indentations).equals ["", "  ", "    ", "      "]
+  asserts(:indentation_count).equals 2
+  asserts(:indentation_value).equals " "
+end
+
+### EDGE CASES ################################################################
+context "#Parser::Indentation - Tab Indented File" do
+  setup do
+    @stdin = "first level\n\tinside first level\n"
+    obj = Sorted::Parser.new(@stdin)
+  end
+
+  asserts(:indentation_count).equals 1
+  asserts(:indentation_value).equals "\t"
+end
