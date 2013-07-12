a6401379352f081aa2bf484673c871550ce00caf
Jon Higgs <jhiggs@eml.cc>
2013-07-12 19:42:17 +0200

    add some checks for dictionary sorting.

--- test/test_sorter.rb
+++ test/test_sorter.rb
@@ -11,14 +11,16 @@ context "#Sorted::Sorter" do
       end
 
       asserts("that it has one element") { topic.size == 1 }
+      asserts("that it can sort by dictionary") { topic.dictionary.size == 1 }
     end
 
     context "with two lines" do
       setup do
-        obj = Sorted::Sorter.new("first_line\nsecond_line\n")
+        obj = Sorted::Sorter.new("z\ny\n")
       end
 
       asserts("that it has two elements") { topic.size == 2 }
+      asserts("that it can sort by dictionary") { topic.dictionary == [{:data=>"y", :depth=>0, :parent=>nil}, {:data=>"z", :depth=>0, :parent=>nil}] }
     end
   end
 
