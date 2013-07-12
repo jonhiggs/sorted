5db0509b2fe89afa0c630b5cec639b976d0a832f
Jon Higgs <jhiggs@eml.cc>
2013-07-12 19:36:05 +0200

    start a sorter class.

---
+++ lib/sorter.rb
@@ -0,0 +1,31 @@
+module Sorted
+  class Sorter
+    def initialize input
+      @input = input
+      @input = ::Sorted::Parser.new(input).to_a if input.is_a?(String)
+    end
+
+    def to_a
+      @input
+    end
+
+    def numeric
+      @input
+    end
+
+    def dictionary
+      @input
+    end
+
+    def json
+      @input
+    end
+
+    def yaml
+      @input
+    end
+
+    def auto
+    end
+  end
+end
---
+++ test/test_sorter.rb
@@ -0,0 +1,25 @@
+require 'parser'
+require 'sorter'
+require 'test_helper'
+
+context "#Sorted::Sorter" do
+
+  context "take a string" do
+    context "with one line" do
+      setup do
+        obj = Sorted::Sorter.new("first_line")
+      end
+
+      asserts("that it has one element") { topic.to_a.size == 1 }
+    end
+
+    context "with two lines" do
+      setup do
+        obj = Sorted::Sorter.new("first_line\nsecond_line\n")
+      end
+
+      asserts("that it has two elements") { topic.to_a.size == 2 }
+    end
+  end
+
+end
