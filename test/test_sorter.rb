59d36cd21eae6b36d2576e1c23ce05048b91a60b
Jon Higgs <jhiggs@eml.cc>
2013-07-13 17:45:17 +0200

    add tests for sorting indented data.

--- test/test_sorter.rb
+++ test/test_sorter.rb
@@ -10,7 +10,7 @@ context "#Sorted::Sorter::Dictionary" do
 
     asserts("to_a returns an array of hashes") { topic.to_a == [{:data=>"y", :depth=>0, :parent=>nil}] }
     asserts("to_s returns data in a string") { topic.to_s == "y\n" }
-    asserts("size is 1") { topic.size == 1 }
+    asserts("size is correct") { topic.size == 1 }
   end
 
   context "can sort flat data" do
@@ -20,6 +20,16 @@ context "#Sorted::Sorter::Dictionary" do
 
     asserts("to_a returns an array of hashes") { topic.to_a == [{:data=>"y", :depth=>0, :parent=>nil}, {:data=>"z", :depth=>0, :parent=>nil}] }
     asserts("to_s returns data in a string") { topic.to_s == "y\nz\n" }
-    asserts("size is 1") { topic.size == 2 }
+    asserts("size is correct") { topic.size == 2 }
+  end
+
+  context "can indented data" do
+    setup do
+      obj = Sorter::Dictionary.new("z\n\ty\n")
+    end
+
+    asserts("to_a returns an array of hashes") { topic.to_a == [{:data=>"z", :depth=>0, :parent=>nil}, {:data=>"y", :depth=>0, :parent=>nil}] }
+    asserts("to_s returns data in a string") { topic.to_s == "z\n\ty\n" }
+    asserts("size is correct") { topic.size == 2 }
   end
 end
