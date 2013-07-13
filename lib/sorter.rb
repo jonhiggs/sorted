ed874111d2e075e984c3ee9d1927aceb0439cea8
Jon Higgs <jhiggs@eml.cc>
2013-07-13 17:44:53 +0200

    move the sorting methods into a module.

--- lib/sorter.rb
+++ lib/sorter.rb
@@ -1,16 +1,23 @@
 class Sorter
+  module Sort
+    def sort
+      @elements.to_a.sort_by{|e| e[:data] }
+    end
+
   class Dictionary
     require 'indentation'
     include Sorted::Formatter
+    include Sorter::Sort
 
     def initialize input, config={}
       @indentation = Sorted::Indentation.new(input)
       @elements = Sorted::Parser.new(input)
-      @elements = sort
-    end
 
-    def sort
-      @elements.to_a.sort_by{|e| e[:data] }
+      @config = {
+        :type => "dictionary"
+      }
+
+      @elements = sort
     end
   end
 end
